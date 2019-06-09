b01e82424463799ca48a1b62966600cde1092f61
alantao5056 <alantao5056@gmail.com>
2019-06-09 19:52:42 -0400

    editroconfig

--- src/index.js
+++ src/index.js
@@ -3,31 +3,31 @@ const calculator = require('./calculator');
 const parser = require('./parser');
 
 const main = async function () {
-    var equationString = await getInput(`What's your fraction equation?`);
-    const equation = parser.parseEquation(equationString);
-    const firstF = parser.parseFraction(equation.firstFraction);
-    const secondF = parser.parseFraction(equation.secondFraction);
-    let result;
-    if (equation.operator === "+") {
-        result = calculator.addFraction(firstF, secondF);
-    } else if (equation.operator === "-") {
-        // result = calculator
-    }
+  var equationString = await getInput(`What's your fraction equation?`);
+  const equation = parser.parseEquation(equationString);
+  const firstF = parser.parseFraction(equation.firstFraction);
+  const secondF = parser.parseFraction(equation.secondFraction);
+  let result;
+  if (equation.operator === "+") {
+    result = calculator.addFraction(firstF, secondF);
+  } else if (equation.operator === "-") {
+    // result = calculator
+  }
 
-    console.log(`${equationString}=${result.numerator}/${result.denominator}`);
+  console.log(`${equationString}=${result.numerator}/${result.denominator}`);
 };
 
 const getInput = async function (message) {
-    let input = await prompt({
-        type: 'input',
-        name: 's',
-        message
-    });
-    return input.s;
+  let input = await prompt({
+    type: 'input',
+    name: 's',
+    message
+  });
+  return input.s;
 };
 
 main().catch((error) => {
-    console.log(error);
+  console.log(error);
 }).then(() => {
-    process.exit();
+  process.exit();
 });
--- src/parser.js
+++ src/parser.js
@@ -1,22 +1,22 @@
 const parseFraction = function (fractionString) {
-    const slashIndex = fractionString.indexOf('/');
-    const numerator = fractionString.substring(0, slashIndex);
-    const denominator = fractionString.substring(slashIndex + 1, fractionString.length);
-    return {
-        numerator: parseInt(numerator),
-        denominator: parseInt(denominator)
-    };
+  const slashIndex = fractionString.indexOf('/');
+  const numerator = fractionString.substring(0, slashIndex);
+  const denominator = fractionString.substring(slashIndex + 1, fractionString.length);
+  return {
+    numerator: parseInt(numerator),
+    denominator: parseInt(denominator)
+  };
 };
 
 const parseEquation = function (equationString) {
-    const operatorIndex = equationString.indexOf('+');
-    const firstFraction = equationString.substring(0, operatorIndex);
-    const secondFraction = equationString.substring(operatorIndex + 1, equationString.length);
-    return {
-        firstFraction: firstFraction,
-        secondFraction: secondFraction,
-        operator: '+'
-    };
+  const operatorIndex = equationString.indexOf('+');
+  const firstFraction = equationString.substring(0, operatorIndex);
+  const secondFraction = equationString.substring(operatorIndex + 1, equationString.length);
+  return {
+    firstFraction: firstFraction,
+    secondFraction: secondFraction,
+    operator: '+'
+  };
 };
 
 module.exports = { parseFraction, parseEquation };
